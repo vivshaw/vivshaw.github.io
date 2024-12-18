1316ec0cfaceaeacb1ca0b9a2388f6e756d6497b
vivshaw <hey@vivsha.ws>
2024-12-18 01:23:44 -0500

    chore: remove dead code and improve appearance of article hero

--- templates/Article/Article.Hero.tsx
+++ templates/Article/Article.Hero.tsx
@@ -73,7 +73,7 @@ const Header = styled.header`
 const HeroHeading = styled(Headings.h1)`
   font-size: 48px;
   font-family: ${(p) => p.theme.fonts.serif};
-  margin-bottom: 25px;
+  margin-bottom: 18px;
   line-height: 1.32;
 
   ${mediaqueries.tablet`
@@ -102,35 +102,4 @@ const HeroSubtitle = styled.div`
       margin-bottom: 5px;
     }
   `}
-`;
-
-// TODO: Fix image height on mobile!!
-const HeroImage = styled.div`
-  position: relative;
-  z-index: 1;
-  width: 100%;
-  max-width: 944px;
-  height: 426px;
-  overflow: hidden;
-  margin: 0 auto;
-  box-shadow: 0 30px 60px -10px rgba(0, 0, 0, 0.2),
-    0 18px 36px -18px rgba(0, 0, 0, 0.22);
-
-  & > img {
-    object-fit: cover;
-  }
-
-  ${mediaqueries.tablet`
-    max-width: 100%;
-  `}
-
-  ${mediaqueries.phablet`
-    margin: 0 auto;
-    width: calc(100vw - 40px);
-    height: 220px;
-
-    & > div {
-      height: 220px;
-    }
-`}
 `;
