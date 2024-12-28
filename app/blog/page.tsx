efe2fe6f02dd788da8089ef8e9f6f1929f9661c3
vivshaw <hey@vivsha.ws>
2024-12-27 21:19:05 -0500

    chore: port blog page to app router

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,17 +4,13 @@ import { getAllBlogPosts } from "@utils/server"
 /**
  * The index page for the blog. Lists all the articles.
  */
-export default function Index({ articlesDateless }) {
+export default async function Blog() {
+  const articlesDateless = await getAllBlogPosts()
+
   const articles = articlesDateless.map((item) => ({
     ...item,
     date: new Date(item.date),
   }))
 
   return <BlogTemplate articles={articles} />
-}
-
-export async function getStaticProps() {
-  const articlesDateless = await getAllBlogPosts()
-
-  return { props: { articlesDateless } }
 }
