78b50eca0cd766d864b6952936c77651331fdd66
vivshaw <hey@vivsha.ws>
2026-02-02 03:32:06 -0500

    feat(MDX, styles): port Headings over to CSS Modules

--- packages/mdx/Headings/Headings.tsx
+++ packages/mdx/Headings/Headings.tsx
@@ -1,47 +1,47 @@
 import { Heading } from "@vivshaw/basalt/components"
 
-import { h1Style, h2Style, h3Style } from "./Headings.css"
+import styles from "./Headings.module.css"
 
 /**
  * a styled `<h1>` element, for use in MDX.
  */
 export function h1({ ...restProps }: React.HTMLAttributes<HTMLHeadingElement>) {
-  return <Heading level="1" className={h1Style} {...restProps} />
+  return <Heading level="1" className={styles.h1} {...restProps} />
 }
 
 /**
  * a styled `<h2>` element, for use in MDX.
  */
 export function h2({ ...restProps }: React.HTMLAttributes<HTMLHeadingElement>) {
-  return <Heading level="2" className={h2Style} {...restProps} />
+  return <Heading level="2" className={styles.h2} {...restProps} />
 }
 
 /**
  * a styled `<h3>` element, for use in MDX.
  */
 export function h3({ ...restProps }: React.HTMLAttributes<HTMLHeadingElement>) {
-  return <Heading level="3" className={h3Style} {...restProps} />
+  return <Heading level="3" className={styles.h3} {...restProps} />
 }
 
 /**
  * a styled `<h4>` element, for use in MDX. Uses the same style as h3.
  */
 export function h4({ ...restProps }: React.HTMLAttributes<HTMLHeadingElement>) {
-  return <Heading level="4" className={h3Style} {...restProps} />
+  return <Heading level="4" className={styles.h3} {...restProps} />
 }
 
 /**
  * a styled `<h5>` element, for use in MDX. Uses the same style as h3.
  */
 export function h5({ ...restProps }: React.HTMLAttributes<HTMLHeadingElement>) {
-  return <Heading level="5" className={h3Style} {...restProps} />
+  return <Heading level="5" className={styles.h3} {...restProps} />
 }
 
 /**
  * a styled `<h6>` element, for use in MDX. Uses the same style as h3.
  */
 export function h6({ ...restProps }: React.HTMLAttributes<HTMLHeadingElement>) {
-  return <Heading level="6" className={h3Style} {...restProps} />
+  return <Heading level="6" className={styles.h3} {...restProps} />
 }
 
 export const headings = {
