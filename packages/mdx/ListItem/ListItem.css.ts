ecf66fcf9bf605b34d140676c41822c795b84234
vivshaw <hey@vivsha.ws>
2026-02-02 03:38:20 -0500

    chore(MDX, styles): remove unused styles in ListItem

--- packages/mdx/ListItem/ListItem.css.ts
+++
@@ -1,6 +0,0 @@
-import { style } from "@vanilla-extract/css"
-
-/**
- * A styled `<li>` element, for use in MDX.
- */
-export const listItem = style({})
--- packages/mdx/ListItem/ListItem.tsx
+++ packages/mdx/ListItem/ListItem.tsx
@@ -1,14 +1,7 @@
-import { clsx } from "clsx"
-
-import { listItem } from "./ListItem.css"
-
 /**
  * a styled `<li>` element, for use in MDX lists.
  * works with both ordered and unordered lists.
  */
-export function ListItem({
-  className,
-  ...restProps
-}: React.LiHTMLAttributes<HTMLLIElement>) {
-  return <li {...restProps} className={clsx(listItem, className)} />
+export function ListItem(props: React.LiHTMLAttributes<HTMLLIElement>) {
+  return <li {...props} />
 }
