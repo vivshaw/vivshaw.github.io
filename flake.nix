8bf81b03a7d378b9cc66deb88acd9551faa38161
vivshaw <hey@vivsha.ws>
2026-05-14 23:04:21 -0400

    feat: nixify

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "vivshae's webbed sight";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_24
+            yarn-berry
+            gh
+            git
+          ];
+        };
+      });
+}
