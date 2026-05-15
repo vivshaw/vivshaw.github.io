41a3db9c16d18266a4b68d5e19ee0c85cbe5ddbc
vivshaw <hey@vivsha.ws>
2026-05-14 20:52:19 -0400

    chore: adopt recess order for CSS

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('stylelint').Config} */
 export default {
-  extends: ["stylelint-config-standard"],
+  extends: ["stylelint-config-standard", "stylelint-config-recess-order"],
   rules: {
     // allow PostCSS at-rules
     "at-rule-no-unknown": [
